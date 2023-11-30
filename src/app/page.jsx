cdbd0d45b943d0f46ec00e4562c44884c7ff6e06
Kris Hvattum <klhvattum@live.com>
2023-11-29 21:46:23 -0800

    layout home page images/blurbs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,8 @@
-import Image from 'next/image'
-import styles from './page.module.scss'
-import { Roboto, Inter } from 'next/font/google'
-import Banner from '@/components/Banner/Banner'
+import Image from 'next/image';
+import styles from './page.module.scss';
+import Button from '@mui/material/Button';
+import { Roboto, Inter } from 'next/font/google';
+import Banner from '@/components/Banner/Banner';
 
 const inter = Inter({ subsets: ['latin'] })
 const roboto = Roboto({
@@ -25,38 +26,41 @@ export default function Home() {
                 strength='400'
             />
             <section className='page-content'>
+
+                {/* LOOK AT CHUCK'S SITE FOR HOW TO LAYOUT TEXT/IMAGES (FAMILY HISTORY SECTION I THINK....) */}
                 <div className={styles.introDiv}>
                     <div className={styles.introText}>
                         <h4><strong>The best of both worlds</strong></h4>
                         <p>
-                            Kardia Classical School combines the benefits of homeschooling and private school education.
+                            Kardia Classical School combines the benefits of homeschooling and private school education. We call this a <em><strong>parent partnership</strong></em>.
                         </p>
-
+                        <Button variant="contained" href='/parent-partnership'>Learn More</Button>
                     </div>                    
                     <Image
                         src='/images/sitting.png'
                         alt='Kardia students sitting on the floor next to a treehouse.'
                         width='960'
                         height='720'
-                        className={styles.sittingImg}
+                        className={`${styles.sittingImg} ${styles.homeImg}`}
                     />
 
                 </div>
                 <div className={styles.introDiv}>
-                    <div className={styles.introText}>
-                        
-                        <h4><strong>Our Mission</strong></h4>
-                        <p>
-                        Kardia Classical School aims to provide parents with the tools that they need to classically educate their children rooted in a Christian worldview. Together, parents and teachers will nurture students’ appreciation of truth, goodness, and beauty as they strive for excellence, while guiding them to live purposefully in the service of God and man.
-                        </p>
-                    </div>
                     <Image
                         src='/images/bubbles.png'
                         alt='Kardia students posing with their PE teacher under a baloon arch'
                         width='640'
                         height='410'
-                        className={styles.bubblesImg}
-                    />                    
+                        className={`${styles.bubblesImg} ${styles.homeImg}`}
+                    />                       
+                    <div className={styles.introText}>
+                        <h4><strong>Our Mission</strong></h4>
+                        <p>
+                        Kardia Classical School aims to provide parents with the tools that they need to classically educate their children in a way that is rooted in a Christian worldview. Together, parents and teachers will nurture students’ appreciation of truth, goodness, and beauty as they strive for excellence.
+                        </p>
+                        <Button variant='contained' href='/programs'>Program Options</Button>
+                    </div>
+               
                  
 
 
