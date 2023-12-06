a16b6abe0265fc7de292677613398ed6e2a53c2b
Kris Hvattum <klhvattum@live.com>
2023-12-06 15:35:19 -0800

    fix escaped characters in august2023 blog

--- src/app/blog/august2023/page.jsx
+++ src/app/blog/august2023/page.jsx
@@ -20,7 +20,7 @@ const page = () => {
             <article className={`page-content ${styles.blogContent}`}>
                 <AnimateOpacity>
                     <section className={`${styles.section} ${lato.className}`}>
-                        <h4 className={styles.blogTitle}>Classical Roots: The Importance of "The Lost Tools of Learning" by Dorothy Sayers</h4>
+                        <h4 className={styles.blogTitle}>Classical Roots: The Importance of &#34;The Lost Tools of Learning&#34; by Dorothy Sayers</h4>
                         <p className={styles.author}>August 29, 2023 by Kris Hvattum</p>
                         <figure>
                             <Image
@@ -41,7 +41,7 @@ const page = () => {
                         <section className={styles.section}>
 
                             <p>
-                                In the realm of educational philosophy, certain texts stand as timeless guides, continually shedding light on the path to effective and meaningful learning. One such beacon is Dorothy Sayers' essay, "The Lost Tools of Learning." Written in 1947, this essay remains a cornerstone of the classical education revival and offers profound insights into the art of teaching and learning. Let's delve into the importance of "The Lost Tools of Learning" and why it continues to resonate with educators, parents, and students alike.
+                                In the realm of educational philosophy, certain texts stand as timeless guides, continually shedding light on the path to effective and meaningful learning. One such beacon is Dorothy Sayers' essay, <em>The Lost Tools of Learning.</em> Written in 1947, this essay remains a cornerstone of the classical education revival and offers profound insights into the art of teaching and learning. Let's delve into the importance of <em>The Lost Tools of Learning</em> and why it continues to resonate with educators, parents, and students alike.
                             </p>
                         </section>
                     </AnimateOpacity>
@@ -51,7 +51,7 @@ const page = () => {
                             <ol>
                                 <li>
                                     <h5>Reviving Classical Education</h5>
-                                    <p>Personalized Learning: AI enables personalized learning experiences tailored to individual students' needs. It can analyze strengths, weaknesses, and learning styles to provide customized content, helping students stay engaged and motivated.</p>
+                                    <p>Personalized Learning: AI enables personalized learning experiences tailored to individual students&#39; needs. It can analyze strengths, weaknesses, and learning styles to provide customized content, helping students stay engaged and motivated.</p>
                                 </li>
                                 <li>
                                     <h5>Three-Part Curriculum</h5>
@@ -62,7 +62,7 @@ const page = () => {
                                 </li>
                                 <li>
                                     <h5>Lifelong Learning</h5>
-                                    <p>"The Lost Tools of Learning" emphasizes the importance of teaching students how to learn, rather than simply what to learn. This skill is invaluable in a rapidly changing world where adaptability and problem-solving are highly prized.</p>
+                                    <p><em>The Lost Tools of Learning</em> emphasizes the importance of teaching students how to learn, rather than simply what to learn. This skill is invaluable in a rapidly changing world where adaptability and problem-solving are highly prized.</p>
                                 </li>
                                 <li>
                                     <h5>Integration of Subjects</h5>
@@ -74,12 +74,12 @@ const page = () => {
                                 </li>
                                 <li>
                                     <h5>Educational Reform</h5>
-                                    <p>"The Lost Tools of Learning" has played a pivotal role in educational reform efforts worldwide. It has inspired the establishment of classical schools and homeschooling curricula that adhere to Sayers' principles.</p>
+                                    <p><em>The Lost Tools of Learning</em> has played a pivotal role in educational reform efforts worldwide. It has inspired the establishment of classical schools and homeschooling curricula that adhere to Sayers' principles.</p>
                                 </li>
 
                                 <li>
                                     <h5>Adaptation to Modern Times</h5>
-                                    <p>While Sayers' essay was written in a different era, its principles remain adaptable to modern educational contexts. The focus on critical thinking, effective communication, and lifelong learning aligns with the demands of the 21st century.</p>
+                                    <p>While Sayers&#39; essay was written in a different era, its principles remain adaptable to modern educational contexts. The focus on critical thinking, effective communication, and lifelong learning aligns with the demands of the 21st century.</p>
                                 </li>
 
                             </ol>
@@ -87,7 +87,7 @@ const page = () => {
                     </AnimateOpacity>
                     <AnimateOpacity>
                         <section className={styles.section}>
-                            <p>In conclusion, "The Lost Tools of Learning" by Dorothy Sayers continues to be an influential work that challenges traditional educational paradigms and offers a roadmap for a more effective and meaningful approach to learning. Its enduring relevance lies in its promotion of critical thinking, integration of knowledge, and the cultivation of well-rounded individuals. As educators, parents, and students continue to seek innovative ways to navigate the complex landscape of education, Sayers' essay remains a beacon of wisdom and a source of inspiration.</p>
+                            <p>In conclusion, <em>The Lost Tools of Learning</em> by Dorothy Sayers continues to be an influential work that challenges traditional educational paradigms and offers a roadmap for a more effective and meaningful approach to learning. Its enduring relevance lies in its promotion of critical thinking, integration of knowledge, and the cultivation of well-rounded individuals. As educators, parents, and students continue to seek innovative ways to navigate the complex landscape of education, Sayers&#39; essay remains a beacon of wisdom and a source of inspiration.</p>
                         </section>
                     </AnimateOpacity>
                     <AnimateOpacity>
