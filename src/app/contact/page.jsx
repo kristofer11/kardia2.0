9147336e3a1b77118a9b63506acc155b4b39d3fc
Kris Hvattum <klhvattum@live.com>
2024-02-14 16:45:45 -0800

    add contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@mui/material';
 import AnimateOpacity from '@/components/animations/AnimateOpacity';
+import ContactForm from '@/components/contactForm/ContactForm';
 
 const page = () => {
     return (
@@ -23,36 +24,39 @@ const page = () => {
                 <section className={`page-content ${styles.contactContent}`} style={{ maxWidth: '98vw' }}>
                     <h2 className='text-center' style={{ color: '#152d7d', fontSize: '2.8rem' }}>We would love to hear from you!</h2>
                     <div className={styles.contactDiv}>
-                        <Image 
+                        <Image
                             height='555'
                             width='555'
                             src='/images/prek-playground.jpeg'
                             alt='Pre Kindergarten students playing on playground'
                         />
                         <div className={styles.linkDiv}>
-                            <h4 className='m-2'>You can reach us via phone, email or Facebook Messenger</h4>
-                            <h5 className='m-2'>(Look for a the link for Messenger in the lower right corner of any page.)</h5>
+                            {/* <h4>You can reach us via the contact form, phone, email or Facebook Messenger</h4> */}
+                            {/* <h5 className='m-2'>(Look for a the link for Messenger in the lower right corner of any page.)</h5> */}
 
-                            <div className={styles.contactText} style={{ fontSize: '1.3rem' }}>
-                                <span>Email:</span>
-                                <span className={styles.contactLink} id='contact-link'>
-                                    <Link variant='contained' href='mailto: office@kardiaclassical.org'> office@KardiaClassical.org</Link>
-                                </span>
+                            <div className={styles.formDiv}>
+                                <ContactForm />
                             </div>
 
-                            <div className={styles.contactText} style={{ fontSize: '1.3rem' }}>
-                                <span>Phone:</span>
-                                <span className={styles.contactLink}>
-                                    <Link variant='contained' href='tel: 360-453-7334'> 360-453-7334</Link>
-                                </span>
-                            </div>
                         </div>
                     </div>
-
-
-
                 </section>
             </AnimateOpacity>
+            <section>
+                <div className={styles.contactText} style={{ fontSize: '1.3rem', marginTop: '3rem' }}>
+                    <span>Email:</span>
+                    <span className={styles.contactLink} id='contact-link'>
+                        <Link variant='contained' href='mailto: office@kardiaclassical.org'> office@KardiaClassical.org</Link>
+                    </span>
+                </div>
+
+                <div className={styles.contactText} style={{ fontSize: '1.3rem', marginBottom: '3rem' }}>
+                    <span>Phone:</span>
+                    <span className={styles.contactLink}>
+                        <Link variant='contained' href='tel: 360-453-7334'> 360-453-7334</Link>
+                    </span>
+                </div>
+            </section>
         </main>
     )
 }
---
+++ src/components/contactForm/ContactForm.jsx
@@ -0,0 +1,63 @@
+"use client"
+import './contactForm.scss';
+import { useForm, ValidationError } from '@formspree/react';
+import Button from '@mui/material/Button';
+
+function ContactForm() {
+    const [state, handleSubmit] = useForm("xnqylggz");
+    if (state.succeeded) {
+        return <p>Thanks for contacting Kardia! We will reply as soon as possible.</p>;
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input
+                id="name"
+                type="text"
+                name="name"
+                required
+            />
+            <ValidationError
+                prefix="Name"
+                field="name"
+                errors={state.errors}
+            />
+            <label htmlFor="email" className='mt-2'>
+                Email Address
+            </label>
+            <input
+                id="email"
+                type="email"
+                name="email"
+                required
+            />
+            <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+            />
+            <label htmlFor="message" className='mt-2'>
+                Message
+            </label>
+            <textarea
+                id="message"
+                name="message"
+                required
+                className='message-input'
+            />
+            <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+            />
+            <Button type="submit" disabled={state.submitting} variant='contained' className='mt-3'>Submit</Button>
+
+        </form>
+    );
+}
+// function App() {
+//     return (
+//         <ContactForm />
+//     );
+// }
+export default ContactForm;
