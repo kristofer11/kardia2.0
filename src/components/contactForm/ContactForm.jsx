50de22d158b092708b5b19974c43b385517cb921
Kris Hvattum <klhvattum@live.com>
2024-02-22 15:48:34 -0800

    Fix broken contact form, add honeypot

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,16 +1,33 @@
 "use client"
 import './contactForm.scss';
+import { useState } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import Button from '@mui/material/Button';
 
 function ContactForm() {
     // const [captcha, getCaptchaProps] = useCaptcha();
-    const [state, handleSubmit] = useForm("xnqylggz");
-    if (state.succeeded) {
+    const formspreeApiKey = FORMSPREE_API_KEY;
+
+    const [state, handleSubmit] = useForm(`${formspreeApiKey}`);
+    const [potFilled, setPotFilled] = useState('false')
+
+    const submitForm = async (event) => {
+        event.preventDefault();
+        const honeypotField = document.getElementById('subject');
+        if (honeypotField.value) {
+            console.log("Honeypot field filled. Likely a bot submission.");
+            setPotFilled(true);
+            console.log(formspreeApiKey)
+            return;
+        }
+        handleSubmit(event);
+    }
+
+    if (state.succeeded || potFilled == true) {
         return <p>Thanks for contacting Kardia! We will reply as soon as possible.</p>;
     }
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={submitForm}>
             <label htmlFor="name">Name:</label>
             <input
                 id="name"
@@ -52,14 +69,26 @@ function ContactForm() {
                 errors={state.errors}
             />
 
+            {/* HONEY POT */}
+            <div style={{ position: 'absolute', left: '-9999px' }}>
+                <label htmlFor="subject">Make sure you fill this in.</label>
+                <input
+                    id="subject"
+                    type="text"
+                    name="subject"
+                />
+            </div>
+
+
             <Button type="submit" disabled={state.submitting} variant='contained' className='mt-3'>Submit</Button>
 
         </form>
     );
 }
-// function App() {
-//     return (
-//         <ContactForm />
-//     );
-// }
-export default ContactForm;
+function App() {
+    return (
+        <ContactForm />
+    );
+}
+export default App;
+
