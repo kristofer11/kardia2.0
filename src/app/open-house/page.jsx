4ee82680509610feefe1b5aaa1dc4eb7573220c5
Kris Hvattum <klhvattum@live.com>
2024-12-27 16:18:53 -0800

    add rsvp button

--- src/app/open-house/page.jsx
+++ src/app/open-house/page.jsx
@@ -1,7 +1,7 @@
 import Banner from '@/components/Banner/Banner';
 import styles from './page.module.scss';
 import Image from 'next/image';
-import Link from 'next/link';
+import Button from '@mui/material/Button';
 
 const page = () => {
     return (
@@ -12,12 +12,19 @@ const page = () => {
                 title={
                     <>
                         <h1 className='overlay-text'>Open House</h1>
-                        <h1 className='overlay-text' style={{ fontSize: '3rem', marginLeft: '4rem' }}><em>January 31st</em></h1>
+                        <h1 className='overlay-text' style={{ fontSize: '3rem', marginLeft: '4rem' }}><em>January 31, 2025</em></h1>
                     </>
                 }
                 strength='400'
             />
-            <div className={styles.auctionImgDiv}>
+            <div className={styles.imgDiv}>
+                <Button
+                    href='https://na01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.signupgenius.com%2Fgo%2F10C0B4AA8AD2EA3FBC43-54151897-january&data=05%7C02%7C%7C831523a97f18442baf7008dd26d060ea%7C84df9e7fe9f640afb435aaaaaaaaaaaa%7C1%7C0%7C638709398649610833%7CUnknown%7CTWFpbGZsb3d8eyJFbXB0eU1hcGkiOnRydWUsIlYiOiIwLjAuMDAwMCIsIlAiOiJXaW4zMiIsIkFOIjoiTWFpbCIsIldUIjoyfQ%3D%3D%7C0%7C%7C%7C&sdata=V6%2FmqQzjWrCFp35islHmtfem1vzdD9rRX5QxZI3Xrys%3D&reserved=0'
+                    variant='contained' 
+                    target='_blank'
+                >
+                   Click here to RSVP
+                </Button>
                 <Image
                     src='/images/open_house.png'
                     width='900'
@@ -25,6 +32,7 @@ const page = () => {
                     alt='information about the 2024 annual auction supporting Kardia Classical School'
                     className={styles.auctionImg}
                 />
+
                 {/* <h3>Open from Thursday March 21 at 7pm to Sunday March 24th at 7pm</h3> */}
             </div>
 
