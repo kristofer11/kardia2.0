584036d95996a0374c5d8d2e02952004d93dac39
Kris Hvattum <klhvattum@live.com>
2025-02-02 19:24:38 -0800

    update auction page

--- src/app/auction/page.jsx
+++ src/app/auction/page.jsx
@@ -7,25 +7,27 @@ const page = () => {
     return (
         <main className={`${styles.main} page-main`}>
             <Banner
-                img='/banners/poppies.jpg'
+                img='/banners/hearts1.png'
                 alt='Lightbulbs over a bookshelf in a library'
                 title={
                     <>
                         <h1 className='overlay-text'>Annual Auction</h1>
-                        <h1 className='overlay-text' style={{ fontSize: '3rem', marginLeft: '4rem' }}><em>In Bloom</em></h1>
+                        <h1 className='overlay-text' style={{ fontSize: '3rem', marginLeft: '4rem' }}><em>Heart of Gold</em></h1>
                     </>
                 }
                 strength='400'
             />
+            
             <div className={styles.auctionImgDiv}>
-                <Image
-                    src='/images/auction.jpg'
+                <Link href='https://fundraiser.support/KardiaClassical2025' target='_blank' ><Image
+                    src='/images/2025_auction.jpg'
                     width='900'
                     height='900'
                     alt='information about the 2024 annual auction supporting Kardia Classical School'
                     className={styles.auctionImg}
-                />
-                <h3>Open from Thursday March 21 at 7pm to Sunday March 24th at 7pm</h3>
+                /></Link>
+                <h4>Click on the image to go to the auction site.</h4>
+                <h3>Open from Friday March 21st at 7pm to Sunday March 23rd at 7pm</h3>
             </div>
 
         </main>
