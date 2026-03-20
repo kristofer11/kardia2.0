eeadbc789f69db0487606d41a3d2b9cc87fc9841
Kris Hvattum <klhvattum@live.com>
2026-03-19 21:23:40 -0700

    fix calendar pdf file name

--- src/app/calendar/page.jsx
+++ src/app/calendar/page.jsx
@@ -13,18 +13,18 @@ const page = () => {
                     <section className={styles.section}>
                         <h1>Calendar</h1>
                         <p >
-                            Here is the calendar for the 2026-2027 School Year! Please keep in mind that dates are subject to change, however, we will give plenty of notice if anything needs to be adjusted. Whenever there are changes to the calendar, it will be updated here as well.<br /><strong>Click on the calendar below to <Link href='downloads/calendar-26.pdf' download='2025-2026 Kardia Calendar.pdf' target='_blank'>download</Link>.</strong>
+                            Here is the calendar for the 2026-2027 School Year! Please keep in mind that dates are subject to change, however, we will give plenty of notice if anything needs to be adjusted. Whenever there are changes to the calendar, it will be updated here as well.<br /><strong>Click on the calendar below to <Link href='/downloads/2026-2027_Kardia_Calendar.pdf' download='2026-2027 Kardia Calendar.pdf' target='_blank'>download</Link>.</strong>
                         </p>
                         <Link
-                            href='downloads/2025-2026-calendar.pdf'
-                            download='2025-2026 Kardia Calendar.pdf'
+                            href='/downloads/2026-2027_Kardia_Calendar.pdf'
+                            download='2026-2027 Kardia Calendar.pdf'
                             target='_blank'
                         >
                             <Image
                                 src='/images/calendar-26.png'
                                 width='680'
                                 height='880'
-                                alt='2024-25 school calendar'
+                                alt='2026-27 school calendar'
                             />
                         </Link>
                     </section>
